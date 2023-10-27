09ea4af19103280037300ce92ea915729baf3811
Cansu Fidan <cansuf20@gmail.com>
2023-10-27 21:20:29 +0200

    first commit

---
+++ src/helpers/constants.js
@@ -0,0 +1,6 @@
+export const options = {
+    headers: {
+      'X-RapidAPI-Key': '8e5b109c12mshe4af105409eadc1p11b128jsn66090ea8581b',
+      'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+    }
+  };
